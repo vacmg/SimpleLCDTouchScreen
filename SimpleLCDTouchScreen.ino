26d3e9d21036ece9b05d794e28a88dcb83b3a068
vacmg <70143287+vacmg@users.noreply.github.com>
2021-08-21 21:30:26 +0200

    Add test folder

---
+++ SimpleLCDTouchScreen.ino
@@ -0,0 +1,21 @@
+﻿/*
+ Name:		SimpleLCDTouchScreen.ino
+ Created:	19/08/2021 4:41:02
+ Author:	Víctor
+*/
+
+#include <TouchScreen.h>
+#include <LCDWIKI_KBV.h>
+#include <LCDWIKI_GUI.h>
+#include "src/SimpleLCDTouchScreen.h"
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
--- src/SimpleLCDTouchScreen.h
+++ src/SimpleLCDTouchScreen.h
@@ -14,6 +14,47 @@
 	#include "WProgram.h"
 #endif
 
+#include <TouchScreen.h>
+#include <LCDWIKI_KBV.h>
+#include <LCDWIKI_GUI.h>
+
+class Color
+{
+public:
+	static uint16_t to565(byte r, byte g, byte b);
+	static void to888(uint16_t color, byte* r, byte* g, byte* b);
+};
+
+class Button
+{
+public:
+	Button();
+	
+	//Button(uint16_t x, uint16_t y, uint16_t tam_x, uint16_t tam_y, uint16_t borderSize, Color borderColor = Color(0,0,0), Color fillColor = Color(0,0,0));
+
+	static void begin();
+
+	bool isPressed();
+
+	void show();
+
+	//void setShowfunction()
+
+	void move(uint16_t x, uint16_t y);
+
+	void setLabel(char* label, int size);
+
+	void setForegroundColor(Color color);
+
+	void setBackgroundColor(Color color);
+
+private:
+	uint16_t _x;
+	uint16_t _y;
+	uint16_t _tam_x;
+	uint16_t _tam_y;
+	// function pointer customshow
+};
 
 #endif
 
